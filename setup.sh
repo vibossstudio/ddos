d29cd3bcf005b77c2782bcf7939460ce6d59e9ec
dhungx <139793488+dhungx@users.noreply.github.com>
2024-08-10 21:23:14 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,39 +1,68 @@
 #!/bin/bash
 
 # Thông báo bắt đầu cài đặt
-echo "PACKAGES ARE BEING INSTALLED, PLEASE WAIT..."
+echo "Đang cài đặt các gói, vui lòng đợi..."
 
-# Cài đặt thư viện colorama
-pip3 install colorama
+# Cài đặt các gói cần thiết
+pip_install() {
+    pip3 install --upgrade pip
+    pip3 install colorama scapy
+}
+
+# Kiểm tra và cài đặt pip nếu chưa có
+install_pip() {
+    if ! command -v pip3 &> /dev/null; then
+        echo "pip3 chưa được cài đặt. Đang cài đặt pip3..."
+        if [[ "$OSTYPE" == "linux-android"* ]]; then
+            pkg install python-pip -y
+        elif [[ "$OSTYPE" == "linux"* ]]; then
+            apt-get update
+            apt-get install python3-pip -y
+        elif [[ "$OSTYPE" == "darwin"* ]]; then
+            brew install python
+        elif [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
+            echo "Vui lòng cài đặt Python và pip từ https://www.python.org/downloads/"
+            exit 1
+        else
+            echo "Hệ điều hành không được hỗ trợ."
+            exit 1
+        fi
+    fi
+}
 
 # Hỏi người dùng có muốn chạy công cụ từ bất kỳ đâu trên hệ thống hay không
-echo "Do you want to execute DDoS from anywhere on your system? [y/n]: "
+echo "Bạn có muốn chạy DDoS từ bất kỳ đâu trên hệ thống của bạn không? [y/n]: "
 read usr
 
 # Hỏi người dùng đang sử dụng môi trường nào: Termux, iSH, hay Linux
-echo "Are you using Termux, iSH Shell, or Linux? [termux/ish/linux]: "
+echo "Bạn đang sử dụng Termux, iSH Shell, hay Linux? [termux/ish/linux]: "
 read usr_env
 
+# Cài đặt pip nếu cần và các thư viện Python
+install_pip
+pip_install
+
+# Thiết lập môi trường dựa trên sự lựa chọn của người dùng
 if [ "$usr_env" == "termux" ] && [ "$usr" == "y" ]; then
     python=$(command -v python3)
     sed -i "1i \#!$python" ddos.py
     mv ddos.py ddos
     mv ddos /data/data/com.termux/files/usr/bin
-    echo "Setup successfully done. Now you can execute the script by typing 'ddos'."
+    echo "Cài đặt hoàn tất. Bây giờ bạn có thể chạy script bằng cách gõ 'ddos'."
 
 elif [ "$usr_env" == "ish" ] && [ "$usr" == "y" ]; then
     python=$(command -v python3)
     sed -i "1i \#!$python" ddos.py
     mv ddos.py ddos
     mv ddos /usr/local/bin
-    echo "Setup successfully done. Now you can execute the script by typing 'ddos'."
+    echo "Cài đặt hoàn tất. Bây giờ bạn có thể chạy script bằng cách gõ 'ddos'."
 
 elif [ "$usr_env" == "linux" ] && [ "$usr" == "y" ]; then
     linux=$(command -v python3)
     sed -i "1i \#!$linux" ddos.py
     mv ddos.py ddos
     sudo mv ddos /usr/bin
-    echo "Setup successfully done. Now you can execute the script by typing 'ddos'."
+    echo "Cài đặt hoàn tất. Bây giờ bạn có thể chạy script bằng cách gõ 'ddos'."
 
 else
     ./ddos.py
