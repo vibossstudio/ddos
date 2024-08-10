de6da7739fb7efbb6b67a8ee825eac67553f936a
dhungx <139793488+dhungx@users.noreply.github.com>
2024-08-10 21:47:26 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -33,8 +33,8 @@ install_pip() {
 
 install_pip
 
-# Cài đặt thư viện colorama
-pip3 install colorama
+# Cài đặt thư viện colorama và scapy
+pip3 install colorama scapy
 
 # Hỏi người dùng có muốn chạy DDoS từ bất kỳ đâu trên hệ thống hay không
 echo "Bạn có muốn chạy DDoS từ bất kỳ đâu trên hệ thống của bạn không? [y/n]: "
